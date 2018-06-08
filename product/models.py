5286a0920efd334168a468bf2c24240d9ec7ac29
ainesmile <ainesmile@gmail.com>
2018-06-08 20:56:49 +1200

    change featured pic_name field type to CharField

--- product/models.py
+++ product/models.py
@@ -4,7 +4,7 @@ class Featured(models.Model):
     name = models.CharField(max_length=200, unique=True)
     fixed_price = models.FloatField(null=True)
     discounted_price = models.FloatField(null=True)
-    pic_name = models.URLField(max_length=200)
+    pic_name = models.CharField(max_length=300)
     size = models.CharField(max_length=30)
     category = models.CharField(max_length=30)
 
