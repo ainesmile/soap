4f9e952e167566b2337e14e9b5a860575bd10fe8
ainesmile <ainesmile@gmail.com>
2018-06-14 12:41:29 +1200

    command `save_product` save products to model

--- product/management/commands/save_product.py
+++ product/management/commands/save_product.py
@@ -2,35 +2,30 @@ from django.core.management.base import BaseCommand, CommandError
 
 from product.models import Product
 import json
+import ast
 
 class Command(BaseCommand):
     help = 'save products'
 
     def add_arguments(self, parser):
-        parser.add_argument('file_name', nargs='?', type=str)
-        parser.add_argument('category', nargs='?', type=str)
+        parser.add_argument('file_names', nargs='?', type=str)
 
     def handle(self, *args, **options):
-        file_name = options['file_name']
-        category = options['category']
+        file_names_str = options['file_names']
+        file_names = ast.literal_eval(file_names_str)
 
-
-        try:
+        for file_name in file_names:
             with open(file_name, 'r') as f:
                 products = json.loads(f.read())
-                
-                for subcategory, product_list in products.items():
-                    for product_item in product_list:
-                        new_product = Product(
-                            category=category,
-                            name=product_item["name"],
-                            fixed_price = product_item["fixed_price"],
-                            discounted_price = product_item["discounted_price"],
-                            pic_name = product_item["pic_name"],
-                            size = product_item["size"],
-                            subcategory = subcategory)
-                        new_product.save()
-        except:
-            raise CommandError("save product failed")
-
+                for product in products:
+                    new_product = Product(
+                        category=product["category"],
+                        name=product["name"],
+                        fixed_price=product["fixed_price"],
+                        discounted_price=product["discounted_price"],
+                        pic_name=product["pic_name"],
+                        size=product["size"],
+                        subcategory=product["subcategory"]
+                    )
+                    new_product.save()
         self.stdout.write('Successfully saved.')
