1ce268e419c87eb45056e081f1a7c63b85ebcb05
ainesmile <ainesmile@gmail.com>
2018-06-15 10:44:48 +1200

    print save info when save_product

--- product/management/commands/save_product.py
+++ product/management/commands/save_product.py
@@ -10,22 +10,43 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('file_names', nargs='?', type=str)
 
+    def check_exist(self, product):
+        kwargs = {
+            "name": product["name"],
+            "size": product["size"],
+            "fixed_price": product["fixed_price"],
+            "category": product["category"],
+            "subcategory": product["subcategory"]
+        }
+        return Product.objects.filter(**kwargs)
+
     def handle(self, *args, **options):
+        # file_names = ["data/best_pics.json", "data/best_swisse.json"]
         file_names_str = options['file_names']
         file_names = ast.literal_eval(file_names_str)
+
+        save_product_number = 0
+        exists_product_number = 0
 
         for file_name in file_names:
             with open(file_name, 'r') as f:
                 products = json.loads(f.read())
                 for product in products:
-                    new_product = Product(
-                        category=product["category"],
-                        name=product["name"],
-                        fixed_price=product["fixed_price"],
-                        discounted_price=product["discounted_price"],
-                        pic_name=product["pic_name"],
-                        size=product["size"],
-                        subcategory=product["subcategory"]
-                    )
-                    new_product.save()
-        self.stdout.write('Successfully saved.')
+                    if self.check_exist(product):
+                        exists_product_number += 1
+                        print(product["name"], 'with', product["size"], 'already exists.')
+                    else:
+                        new_product = Product(
+                            category=product["category"],
+                            name=product["name"],
+                            fixed_price=product["fixed_price"],
+                            discounted_price=product["discounted_price"],
+                            pic_name=product["pic_name"],
+                            size=product["size"],
+                            subcategory=product["subcategory"]
+                        )
+                        new_product.save()
+                        save_product_number += 1
+                        print(product["name"], 'with', product["size"], 'saved.')
+        
+        print('Successfully saved ', save_product_number, '. Already Exists ', exists_product_number)
