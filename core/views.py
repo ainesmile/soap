01ba10fa77f3478e99aa4b4b089151e79c227cad
ainesmile <ainesmile@gmail.com>
2018-06-14 13:06:04 +1200

    abstract product_list

--- core/views.py
+++ core/views.py
@@ -6,5 +6,7 @@ from product.models import Product
 
 class Home(View):
     def get(self, requset):
-        featured = Product.objects.filter(category='F')
-        return render(requset, 'core/home.html', {"featured": featured})
+        product_list = Product.objects.filter(category='F')
+        return render(requset, 'core/home.html', {
+            "product_list": product_list,
+            "categoty_name": 'featured'})
