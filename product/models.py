de8cd199a9576d6ca81af398ac4cb2b4d5e0530d
ainesmile <ainesmile@gmail.com>
2018-06-21 20:33:46 +1200

    change `product` model

--- product/models.py
+++ product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Product(models.Model):
     CATEGORY_CHOICES = (
         ('B', 'Best'),
@@ -7,12 +8,17 @@ class Product(models.Model):
         ('N', 'Normal')
     )
     category = models.CharField(max_length=1, choices=CATEGORY_CHOICES)
+    subcategory = models.CharField(max_length=200)
+    category_3 = models.CharField(max_length=200)
+    category_4 = models.CharField(max_length=200)
+
     name = models.CharField(max_length=200)
     fixed_price = models.FloatField(null=True)
     discounted_price = models.FloatField(null=True)
     pic_name = models.CharField(max_length=300)
     size = models.CharField(max_length=30)
-    subcategory = models.CharField(max_length=30)
 
     class Meta:
-        ordering = ['category', '-subcategory']
+        ordering = ['category', '-subcategory', '-category_3', '-category_4', 'name']
+
+    
